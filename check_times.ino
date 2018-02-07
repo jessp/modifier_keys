c5ec9463943827deb477d8c056b0387f3a884aea
Jess <jessicapeter92@gmail.com>
2018-02-07 15:43:52 +0100

    some fixes to prevent chars entered when lifting fingers from modifier keys

--- check_times.ino
+++ check_times.ino
@@ -15,8 +15,6 @@ void check_alpha() {
   int leftHandCounter = 0;
   int rightHandCounter = 0;
 
-
-  
   
   for (byte index = 0; index < 5; index++) {
     if (pressed[index]) {
@@ -50,22 +48,42 @@ void check_alpha() {
 
   }
 
+//"<" represents a programmed "delete" rather than a user-entered backspace
 if (leftHandCounter != 0 || rightHandCounter != 0){    
     if (leftHandCounter > 1 && rightHandCounter > 1){
-     
-      if (lastChar != 92){
+      if (lastChar != 102){
+        byte rightPins[] = {5, 6, 7, 8, 9};
+        byte leftPins[] = {0, 1, 2, 3, 4};
+        
+        if (!moreThanOnePressed(rightPins) || !moreThanOnePressed(leftPins)){
+           Serial.print("<");
+        }
+        
         lastChar = "/";
         Serial.println("/"); //enter
       }
     } else if (rightHandCounter > 1){
-      if (lastChar != 78){
-        lastChar = " ";
-        Serial.print(" "); //space
+      if (lastChar != 94 && lastChar != 106){
+        byte rightPins[] = {5, 6, 7, 8, 9};
+        //check if this is just residual key press from lifting fingers for enter character
+        if (atLeastOneJustPressed(rightPins)){
+          if (!moreThanOnePressed(rightPins)){
+            Serial.print("<");
+          }
+          lastChar = "]";
+          Serial.print("]"); //space
+        }
       }
     } else if (leftHandCounter > 1){
-      if (lastChar != 94){
-        lastChar = "\\";
-        Serial.print("\\"); //bkspc
+      if (lastChar != 108){
+        byte leftPins[] = {0, 1, 2, 3, 4};
+        if (atLeastOneJustPressed(leftPins)){
+          if (!moreThanOnePressed(leftPins)){
+            Serial.print("<");
+          }
+          lastChar = "[";
+          Serial.print("["); //bkspc
+        }
       }
     }
     
@@ -73,11 +91,9 @@ if (leftHandCounter != 0 || rightHandCounter != 0){
       //it's impossible to press two keys at the same time, so if two keys are pressed more than 200 ms, insert a delete character
       //in order to remove the character inserted by the first pressed character
       
-      if (justPressed[leftHandFinger] && !justPressed[rightHandFinger + 5]){
-            Serial.print("\\");
-      }
-      if (!justPressed[leftHandFinger] && justPressed[rightHandFinger + 5]){
-            Serial.print("\\");
+      if ((justPressed[leftHandFinger] && !justPressed[rightHandFinger + 5]) ||
+          (!justPressed[leftHandFinger] && justPressed[rightHandFinger + 5])){
+            Serial.print("<");
       }
       
       if (justPressed[leftHandFinger] || justPressed[rightHandFinger + 5]){
@@ -105,5 +121,33 @@ if (leftHandCounter != 0 || rightHandCounter != 0){
 }
   
 }
+//it's tricky to press keys entirely simultaneously
+//so, to check if this is a non-simultaneous multi-key press
+//we check if more than one key is flagged as "justPressed"
+boolean moreThanOnePressed(byte pins[]){
+  int count = 0;
+  for (byte pin = 0; pin < 5; pin++){
+    if (justPressed[pins[pin]]){
+      count++;
+      if (count > 1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
+
+boolean atLeastOneJustPressed(byte pins[]){
+  int count = 0;
+  for (byte pin = 0; pin < 5; pin++){
+    if (justPressed[pins[pin]]){
+      count++;
+      if (count > 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
